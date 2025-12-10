322042a2907e6f569c69429b06c8047f430eb806
crmercadosa <crmercadosa@ittepic.edu.mx>
2025-12-10 14:48:09 -0700

    Extraer phone id

--- src/services/webhook.service.js
+++ src/services/webhook.service.js
@@ -1,4 +1,5 @@
 import { sendMessage } from './whatsapp.service.js';
+import 'dotenv/config';
 
 const processIncomingMessage = async (payload) => {
   const entry = payload.entry?.[0];
@@ -6,6 +7,7 @@ const processIncomingMessage = async (payload) => {
   const value = changes?.value;
   const message = value?.messages?.[0];
   const contact = value?.contacts?.[0];
+  const phon_id = value?.metadata?.phone_number_id;
 
   if (!message) return;
 
@@ -13,10 +15,15 @@ const processIncomingMessage = async (payload) => {
   const from = message.from;
   const text = message.text?.body;
   
+  
   console.log('Mensaje recibido de:', name);
   console.log('Numero:', from);
   console.log('Texto:', text);
+  console.log('Phone Number ID:', phon_id);
 
+  // Responder al mensaje recibido
+  const reply = `Hola ${name}, gracias por tu mensaje: "${text}". Te responderemos pronto.`;
+  await sendMessage(from, reply);
 
   /*
     Logica a futuro no muy lejano:
