182a9e4aeb7926ea3b5417b3a3e357645c7e1ec1
crmercadosa <crmercadosa@ittepic.edu.mx>
2025-12-29 10:01:21 -0700

    Prisma

--- src/services/prisma-queries.service.js
+++ src/services/prisma-queries.service.js
@@ -1,22 +1,23 @@
-import { PrismaClient } from '../generated/prisma/client.js';
+  import { PrismaClient } from '../generated/prisma/client.js';
 
-const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-export const findActiveSucursalByPhone = async (phone) => {
-  return prisma.canales.findFirst({
-    where: {
-      numero_telefonico: phone,
-      sucursales: {
-        estado: 'activo'
-      }
-    },
-    select: {
-      sucursales: {
-        select: {
-          id_sucursal: true,
-          nombre_negocio: true
+
+  export const findActiveSucursalByPhone = async (phone) => {
+    return prisma.canales.findFirst({
+      where: {
+        numero_telefonico: phone,
+        sucursales: {
+          estado: 'activo'
+        }
+      },
+      select: {
+        sucursales: {
+          select: {
+            id_sucursal: true,
+            nombre_negocio: true
+          }
         }
       }
-    }
-  });
-};
+    });
+  };
